8cdecbcdae010460210e0b59b847067a199f1152
Haesol <haesol0828@douzone.com>
2022-08-05 10:27:32 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,22 @@
 import './app.css';
+import {useEffect, useState} from "react";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
+    const [videos, setVideos] = useState([]);
+    useEffect(() => {
+        console.log('useEffect');
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBXQapvezFFdXhbP9U-2Z2MbbCDvAzRJQc", requestOptions)
+            .then(response => response.json())
+            .then(result => setVideos(result.items))
+            .catch(error => console.log('error', error));
+    }, []);
+    return <VideoList videos={videos} />
 }
 
 export default App;
