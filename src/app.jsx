f027b2bc32f6b6e3d55dbf9e4d98887e380d67fb
Haesol <haesol0828@douzone.com>
2022-08-08 13:42:24 +0900

    make video mode

--- src/app.jsx
+++ src/app.jsx
@@ -2,25 +2,43 @@ import styles from './app.module.css';
 import {useEffect, useState} from "react";
 import VideoList from "./components/video_list/video_list";
 import SearchHeader from "./components/search_header/search_header";
+import VideoDetail from "./components/video_detail/video_detail";
 
 function App({ youtube }) {
     const [videos, setVideos] = useState([]);
+    const [selectedVideo, setSelectedVideo] = useState(null);
     useEffect(() => {
         youtube
             .mostPopular()
             .then(videos => setVideos(videos));
     }, []);
 
+    const selectVideo = video => {
+        setSelectedVideo(video);
+    }
+
     const search = query => {
         youtube
             .search(query)
-            .then(videos => setVideos(videos));
+            .then(videos => {
+                setVideos(videos);
+                setSelectedVideo(null);
+            }
+    );
     };
-
     return (
         <div className={styles.app}>
             <SearchHeader onSearch={search}/>
-            <VideoList videos={videos}/>
+            <section className={styles.content}>
+                {selectedVideo && (
+                    <div className={styles.detail}>
+                        <VideoDetail video={selectedVideo}/>
+                    </div>
+                )}
+                <div className={styles.list}>
+                    <VideoList videos={videos} onVideoClick={selectVideo} display={selectedVideo ? 'list' : 'grid'}/>
+                </div>
+            </section>
         </div>
     );
 }
--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import styles from './video_item.module.css';
 
-const VideoItem = ({video: {snippet}}) => {
-
-    const handleListClick = () => {
-        // props.onListClick(video);
-    }
+const VideoItem = ({video, video: {snippet}, onVideoClick, display}) => {
+    const displayType = display === 'list' ? styles.list : styles.grid;
 
     return (
         <li
-            className={styles.container}
-            onClick={handleListClick}>
+            className={`${styles.container} ${displayType}`}
+            onClick={() => onVideoClick(video)}>
             <div className={styles.video}>
                 <img
                     className={styles.thumbnail}
